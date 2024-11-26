25d8f30e10fbd8cce920b57db28c470d831171fa
Naisong Zhou <zhounaisong327@126.com>
2024-11-26 13:03:55 +0100

    add backend api for highlight text

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const task1Controller = require('../controllers/pageSummary');
 const task2Controller = require('../controllers/customizedRequest');
 const task3Controller = require('../controllers/sentenceExplanation');
+const task4Controller = require('../controllers/highlightSentence');
 
 router.post('/pageSummarize', task1Controller.pageSummarize);
 router.post('/customizedReq', task2Controller.customizedReq);
 router.post('/sentenceExplain', task3Controller.sentenceExplain);
+router.post('/highlightSentence', task4Controller.highlightSentence);
 
 module.exports = router;

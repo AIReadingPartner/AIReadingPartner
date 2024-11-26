0ee6e2eb3bb3ac8ef8280ac7cc5f6e4acf9fc588
Yixuan Zhou <yixuan.zhou@uzh.ch>
2024-11-26 11:28:25 +0100

    backend - refactor route names

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -5,8 +5,8 @@ const task1Controller = require('../controllers/pageSummary');
 const task2Controller = require('../controllers/customizedRequest');
 const task3Controller = require('../controllers/sentenceExplanation');
 
-router.post('/task1', task1Controller.pageSummarize);
-router.post('/task2', task2Controller.customizedReq);
-router.post('/task3', task3Controller.sentenceExplain);
+router.post('/pageSummarize', task1Controller.pageSummarize);
+router.post('/customizedReq', task2Controller.customizedReq);
+router.post('/sentenceExplain', task3Controller.sentenceExplain);
 
 module.exports = router;

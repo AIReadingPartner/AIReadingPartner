f2981f1b46695b3624b67e08a41ed557ad6940f3
Yixuan Zhou <yixuan.zhou@uzh.ch>
2024-11-26 11:12:30 +0100

    backend - adding the save to db logic to pageSummary and sentenceExplain backend - refactor method names

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -5,8 +5,8 @@ const task1Controller = require('../controllers/pageSummary');
 const task2Controller = require('../controllers/customizedRequest');
 const task3Controller = require('../controllers/sentenceExplanation');
 
-router.post('/task1', task1Controller.processTask1);
-router.post('/task2', task2Controller.processTask2);
-router.post('/task3', task3Controller.processTask3);
+router.post('/task1', task1Controller.pageSummarize);
+router.post('/task2', task2Controller.customizedReq);
+router.post('/task3', task3Controller.sentenceExplain);
 
 module.exports = router;

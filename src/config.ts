1e1bfad20d67cb60ffaa733b4ea84ba3fa66ceac
Naisong Zhou <zhounaisong327@126.com>
2024-12-03 22:32:43 +0100

    add condition for API call

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,12 @@
 const config = {
-  API_URL: 'https://aireadingpartner.onrender.com/api'
-}
-export default config;
+  development: {
+    API_URL: 'http://localhost:3030/api',
+  },
+  production: {
+    // API_URL: 'https://your-remote-api.com/api',
+    API_URL: 'https://aireadingpartner.onrender.com/api'
+
+  },
+};
+
+export default config[(process.env.NODE_ENV as 'development' | 'production') || 'development'];
